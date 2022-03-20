3d9d60a3cc4d9d28234d991a5acc218483dd1d5b
Anderson_Ferreira <88979659+AndersonFerreiraSantos@users.noreply.github.com>
2022-03-20 20:28:17 -0300

    Inicio Jogo 2D PAC III
    Criado base do jogo.
    
    Movimento, gravidade e frame.
    
    Necessário continuar terminar background e desenhar os mapas.

---
+++ Faculdade/3 Semestre/PAC III/Jogo 2D V0.0.1/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+const UP  = Vector2(0, -1)
+var motion = Vector2()
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -550
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$Sprite.play("Run")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
+	else:
+		motion.x = 0
+		$Sprite.play("Idle")
+	if is_on_floor():
+		print("esta no chao")
+		if Input.is_action_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	else:
+		$Sprite.play("Jump")
+			
+
+	motion = move_and_slide(motion, UP)
